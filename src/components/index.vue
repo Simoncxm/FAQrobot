aef14c1e3f5460400323ba098c5a4e17e717e7d7
chuximing <chuximing@qq.com>
2020-05-07 21:16:40 +0800

    'init'

---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>知识图谱生成</h1>
+    <h2>输入文章</h2>
+    <Input v-model="text" type="textarea" :rows="5" placeholder="Enter something..." />
+    <Button @click="postText">提交</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    postText: function () {
+      console.log(this.text);
+    }
+  }
+}
+</script>
